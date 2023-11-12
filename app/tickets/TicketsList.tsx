e3a974d626052058bc70971e7037b3af4d1c9186
abunaserkayes1 <come74321@gmail.com>
2023-11-12 22:43:08 +0600

    Update TicketsList Components

--- app/tickets/TicketsList.tsx
+++ app/tickets/TicketsList.tsx
@@ -1,11 +1,29 @@
-export default async function getTickets() {
-  const res = await fetch("http://localhost:7000/tickets");
+async function getTickets() {
+  const res = await fetch("http://localhost:7000/tickets", {
+    next: {
+      revalidate: 15,
+    },
+  });
   return res.json();
 }
 
-const TicketsList = () => {
-  const tickets = getTickets();
-  return <div>TicketsList</div>;
+const TicketsList = async () => {
+  const tickets = await getTickets();
+
+  return (
+    <>
+      {tickets.map((ticket: any) => (
+        <div className="card my-5" key={ticket.id}>
+          <h3>{ticket.title}</h3>
+          <p>{ticket.body.slice(0, 300)}</p>
+          <div className={`pill ${ticket.priority}`}>{ticket.priority}</div>
+        </div>
+      ))}
+      {tickets.length === 0 && (
+        <p className="text-center">There is no ticket Available Here,Yea!!</p>
+      )}
+    </>
+  );
 };
 
-export { TicketsList };
+export default TicketsList;
--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import TicketsList from "./TicketsList";
 
 const Tickets = () => {
   return (
-    <h1>Tickets</h1>
-  )
-}
+    <>
+      <h1>Tickets</h1>
+      <div>
+        <TicketsList />
+      </div>
+    </>
+  );
+};
 
-export default Tickets
+export default Tickets;
